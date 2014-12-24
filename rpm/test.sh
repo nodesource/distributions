c2e89017216bfb4ea5b22f3cc3e8431b5a261865
Rod Vagg <rod@vagg.org>
2014-12-24 12:41:15 +1100

    fix rpm lookup tests

--- rpm/test.sh
+++ rpm/test.sh
@@ -31,7 +31,7 @@ testCheckDistro () {
   echo "echo ${uname}" > _test_bin/uname
   chmod 755 _test_bin/uname
 
-  _result=$(PATH=_test_bin/:${PATH} ./_test_check_distro.sh)
+  _result=$(PATH=_test_bin/:${PATH} ./_test_check_distro.sh | sed '/^\+.*$/d')
   local result=$_result
 
   if [[ $? -ne 0 ]]; then
@@ -43,7 +43,8 @@ testCheckDistro () {
 
   if [ "X${result}" != "X${expected}" ]; then
     echo -e "\033[1m\033[31mFAIL\033[39m\033[22m: $release $uname (match)"
-  echo $result
+    echo -e "   ${result}"
+    echo -e "!= ${expected}"
     return
   else
     echo -e "\033[1m\033[32mPASS\033[39m\033[22m: $release $uname"
@@ -58,15 +59,16 @@ testCheckDistro  fedora-release-19-8.noarch                        x86_64  fc   
 testCheckDistro  fedora-release-20-3.noarch                        i686    fc       20          i386     https://rpm.nodesource.com/pub/fc/20/i386/nodesource-release-fc20-1.noarch.rpm
 testCheckDistro  fedora-release-20-3.noarch                        x86_64  fc       20          x86_64   https://rpm.nodesource.com/pub/fc/20/x86_64/nodesource-release-fc20-1.noarch.rpm
 
-testCheckDistro  centos-release-5-8.el5.centos                     i686    el       5           i386     https://rpm.nodesource.com/pub/el/5/i386/nodesource-release-5-1.noarch.rpm
-testCheckDistro  centos-release-5-10.el5.centos                    x86_64  el       5           x86_64   https://rpm.nodesource.com/pub/el/5/x86_64/nodesource-release-5-1.noarch.rpm
-testCheckDistro  centos-release-6-5.el6.centos.11.2.i686           i686    el       6           i386     https://rpm.nodesource.com/pub/el/6/i386/nodesource-release-6-1.noarch.rpm
-testCheckDistro  centos-release-6-5.el6.centos.11.2.x86_64         x86_64  el       6           x86_64   https://rpm.nodesource.com/pub/el/6/x86_64/nodesource-release-6-1.noarch.rpm
-testCheckDistro  centos-release-7-0.1406.el7.centos.2.5.x86_64     x86_64  el       7           x86_64   https://rpm.nodesource.com/pub/el/7/x86_64/nodesource-release-7-1.noarch.rpm
+testCheckDistro  centos-release-5-8.el5.centos                     i686    el       5           i386     https://rpm.nodesource.com/pub/el/5/i386/nodesource-release-el5-1.noarch.rpm
+testCheckDistro  centos-release-5-10.el5.centos                    x86_64  el       5           x86_64   https://rpm.nodesource.com/pub/el/5/x86_64/nodesource-release-el5-1.noarch.rpm
+testCheckDistro  centos-release-6-5.el6.centos.11.2.i686           i686    el       6           i386     https://rpm.nodesource.com/pub/el/6/i386/nodesource-release-el6-1.noarch.rpm
+testCheckDistro  centos-release-6-5.el6.centos.11.2.x86_64         x86_64  el       6           x86_64   https://rpm.nodesource.com/pub/el/6/x86_64/nodesource-release-el6-1.noarch.rpm
+testCheckDistro  centos-release-7-0.1406.el7.centos.2.5.x86_64     x86_64  el       7           x86_64   https://rpm.nodesource.com/pub/el/7/x86_64/nodesource-release-el7-1.noarch.rpm
 
-testCheckDistro  redhat-release-5Server-5.10.0.4                   x86_64  el       5           x86_64   https://rpm.nodesource.com/pub/el/5/x86_64/nodesource-release-5-1.noarch.rpm
-testCheckDistro  redhat-release-server-6Server-6.5.0.1.el6.x86_64  x86_64  el       6           x86_64   https://rpm.nodesource.com/pub/el/6/x86_64/nodesource-release-6-1.noarch.rpm
-testCheckDistro  redhat-release-server-7.0-1.el7.x86_64            x86_64  el       7           x86_64   https://rpm.nodesource.com/pub/el/7/x86_64/nodesource-release-7-1.noarch.rpm
+testCheckDistro  redhat-release-5Server-5.10.0.4                   x86_64  el       5           x86_64   https://rpm.nodesource.com/pub/el/5/x86_64/nodesource-release-el5-1.noarch.rpm
+testCheckDistro  redhat-release-server-6Server-6.5.0.1.el6.x86_64  x86_64  el       6           x86_64   https://rpm.nodesource.com/pub/el/6/x86_64/nodesource-release-el6-1.noarch.rpm
+testCheckDistro  redhat-release-server-7.0-1.el7.x86_64            x86_64  el       7           x86_64   https://rpm.nodesource.com/pub/el/7/x86_64/nodesource-release-el7-1.noarch.rpm
+testCheckDistro  redhat-release-workstation-6Workstation-6.5.0.2.el6.x86_64 x86_64 el 6         x86_64   https://rpm.nodesource.com/pub/el/6/x86_64/nodesource-release-el6-1.noarch.rpm
 
 clean
 
