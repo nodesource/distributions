4d477c6b8f2d72d062f366bf1266c0e75ab891c7
Rod Vagg <rod@vagg.org>
2016-06-30 14:55:41 +1000

    update rpm scripts with io.js and v5 warnings

--- rpm/src/build.sh
+++ rpm/src/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-RELEASES=( "pub_0.10::nodejs:Node.js 0.10"
-           "pub_0.10:_0.10:nodejs:Node.js 0.10"
-           "pub_0.12:_0.12:nodejs:Node.js 0.12"
-           "pub_iojs_1.x:_iojs_1.x:iojs:io.js 1.x"
-           "pub_iojs_2.x:_iojs_2.x:iojs:io.js 2.x"
-           "pub_4.x:_4.x:nodejs:Node.js 4.x LTS Argon"
-           "pub_5.x:_5.x:nodejs:Node.js 5.x"
+RELEASES=( "pub_0.10::nodejs:Node.js v0.10"
+           "pub_0.10:_0.10:nodejs:Node.js v0.10"
+           "pub_0.12:_0.12:nodejs:Node.js v0.12"
+           "pub_iojs_1.x:_iojs_1.x:iojs:io.js v1.x"
+           "pub_iojs_2.x:_iojs_2.x:iojs:io.js v2.x"
+           "pub_4.x:_4.x:nodejs:Node.js v4.x LTS Argon"
+           "pub_5.x:_5.x:nodejs:Node.js v5.x"
          )
 SOURCE=_setup.sh
 DEST=../setup
