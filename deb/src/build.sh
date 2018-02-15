beeb348fb7362473cb44a4becda438b5b0d589d4
Chris Lea <chris.lea@gmail.com>
2018-02-15 10:02:20 -0800

    Get deb build script up to date.

--- deb/src/build.sh
+++ deb/src/build.sh
@@ -11,7 +11,8 @@ RELEASES=( "node_0.10::nodejs:Node.js v0.10"
            "node_5.x:_5.x:nodejs:Node.js v5.x"
            "node_6.x:_6.x:nodejs:Node.js v6.x LTS Boron"
            "node_7.x:_7.x:nodejs:Node.js v7.x"
-           "node_8.x:_8.x:nodejs:Node.js v8.x"
+           "node_8.x:_8.x:nodejs:Node.js v8.x LTS Carbon"
+           "node_9.x:_9.x:nodejs:Node.js v9.x"
          )
 SOURCE=_setup.sh
 DEST=../setup
