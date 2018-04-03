2506a4198bb47fdcece4b09ec1260139c1bd5b27
Diogo Bemfica <diogo.fragabemfica@gmail.com>
2018-04-03 09:59:57 -0300

    Add Deepin 15 on check_alt

--- deb/src/_setup.sh
+++ deb/src/_setup.sh
@@ -279,6 +279,7 @@ check_alt "bunsenlabs"    "bunsen-hydrogen" "Debian" "jessie"
 check_alt "Tanglu"        "chromodoris" "Debian" "jessie"
 check_alt "Devuan"        "ceres"    "Debian" "sid"
 check_alt "PureOS"        "green"    "Debian" "sid"
+check_alt "Deepin"        "panda"    "Debian" "sid"
 
 if [ "X${DISTRO}" == "Xdebian" ]; then
   print_status "Unknown Debian-based distribution, checking /etc/debian_version..."
