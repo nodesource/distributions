d99701240ebec30cac27f6f54d18cde3d16c6c7d
Ivan Iguaran <ivigsu@gmail.com>
2020-07-09 15:08:55 -0500

    Add LTS and Current install scripts

--- deb/src/build.sh
+++ deb/src/build.sh
@@ -18,6 +18,8 @@ RELEASES=( "node_0.10::nodejs:Node.js 0.10"
            "node_12.x:_12.x:nodejs:Node.js 12.x"
            "node_13.x:_13.x:nodejs:Node.js 13.x"
            "node_14.x:_14.x:nodejs:Node.js 14.x"
+           "node_12.x:_lts.x:nodejs:Node.js 12.x"
+           "node_14.x:_current.x:nodejs:Node.js 14.x"
          )
 SOURCE=_setup.sh
 DEST=../setup
