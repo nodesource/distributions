e9bda72d86e214dbc6ff547b7af2901891bf6fae
tmarshall <timothyjmarshall@gmail.com>
2022-04-20 10:33:54 -0500

    updated build.sh — added 18.x

--- rpm/src/build.sh
+++ rpm/src/build.sh
@@ -20,8 +20,9 @@ RELEASES=( "pub_0.10::nodejs:Node.js 0.10"
            "pub_15.x:_15.x:nodejs:Node.js 15.x"
            "pub_16.x:_16.x:nodejs:Node.js 16.x"
            "pub_17.x:_17.x:nodejs:Node.js 17.x"
+           "pub_18.x:_18.x:nodejs:Node.js 18.x"
            "pub_16.x:_lts.x:nodejs:Node.js 16.x"
-           "pub_17.x:_current.x:nodejs:Node.js 17.x"
+           "pub_18.x:_current.x:nodejs:Node.js 18.x"
          )
 SOURCE=_setup.sh
 DEST=../setup
