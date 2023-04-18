4f5b959a0f0c53822a2eccd9ed8d9ac79b38b845
Dave Reid <dave@davereid.net>
2023-04-18 13:42:29 -0500

    Update current to 20

--- deb/src/build.sh
+++ deb/src/build.sh
@@ -23,8 +23,9 @@ RELEASES=( "node_0.10::nodejs:Node.js 0.10"
            "node_17.x:_17.x:nodejs:Node.js 17.x"
            "node_18.x:_18.x:nodejs:Node.js 18.x"
            "node_19.x:_19.x:nodejs:Node.js 19.x"
+           "node_20.x:_20.x:nodejs:Node.js 20.x"
            "node_18.x:_lts.x:nodejs:Node.js 18.x"
-           "node_19.x:_current.x:nodejs:Node.js 19.x"
+           "node_20.x:_current.x:nodejs:Node.js 20.x"
          )
 SOURCE=_setup.sh
 DEST=../setup
