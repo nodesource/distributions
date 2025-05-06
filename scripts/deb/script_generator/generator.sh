ba48c1fe6e843e9ffb60aefc5da5d00234c83f04
Jefferson <jefferson.rios.caro@gmail.com>
2025-05-06 11:51:47 -0500

    chore: add nodejs 24 current
    Signed-off-by: Jefferson <jefferson.rios.caro@gmail.com>
    
    chore: add nodejs 24 current

--- scripts/deb/script_generator/generator.sh
+++ scripts/deb/script_generator/generator.sh
@@ -25,7 +25,7 @@ if [ ! -f "$base_script" ]; then
 fi
 
 # List of versions
-versions=("18" "20" "22" "23")
+versions=("18" "20" "22" "23" "24")
 
 # Iterate over the versions and create scripts
 for version in "${versions[@]}"; do
@@ -34,7 +34,7 @@ done
 
 # Define LTS and current Node.js versions
 lts_version="22"
-current_version="23"
+current_version="24"
 
 # Create setup_lts and setup_current scripts
 create_script "$lts_version" "lts"
